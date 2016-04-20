3d4aae0f7da7390080a1a191d3249685d2e12e30
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-04-19 18:24:50 -0700

    added directives! yay

--- brandon-feinstein/app/app.js
+++ brandon-feinstein/app/app.js
@@ -3,29 +3,53 @@ var app = angular.module('app', [])
 app.directive('customNav', function() {
   return {
     return : 'E',
-    // this location is relative to index.html. NOT the current file
     templateUrl: './app/templates/navbar.html'
   }
 })
 
 app.directive('book', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            data: '='
-        },
-        templateUrl: './app/templates/book-widget.html'
-    }
+  return {
+    restrict: 'E',
+    scope: {
+      data: '='
+    },
+    templateUrl: './app/templates/book-widget.html'
+  }
 })
-
 
 app.directive("user", function() {
-    return {
-        restrict: 'E',
-        link: function(scope, element, attrs) {
-            scope.username = attrs.username;
-            scope.avatar = attrs.avatar;
-        },
-        template: '<div>Username: {{username}}, Avatar: <img src ={{avatar}}></div>'
+  return {
+    restrict: 'E',
+    link: function(scope, element, attrs) {
+      scope.username = attrs.username;
+      scope.image = attrs.image;
+    },
+    template: '<div>Username: {{username}}, Pic: <img src ={{image}}></div>'
     }
 })
+
+app.directive('myDomDirective', function () {
+  return {
+    link: function ($scope, element, attrs) {
+      element.bind('click', function () {
+        element.html('ButtonClicked!');
+      });
+      element.bind('mouseenter', function () {
+        element.css('background-color', 'red');
+      });
+      element.bind('mouseleave', function () {
+        element.css('background-color', 'white');
+      });
+    }
+  };
+});
+
+app.directive('myAttribute', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      myAttribute: '='
+    },
+    templateUrl: './app/templates/attribute.html'
+  };
+});
